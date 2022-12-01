2944b204066c52c3ccee4ef4454e62011dba4db6
Lorenzoscajola <117931214+Lorenzoscajola@users.noreply.github.com>
2022-12-01 11:42:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int numero;
+    cin>>numero;
+    if (funzione(numero,2)==1)
+        cout<<"numero primo";
+    else cout<<"numero non primo";
+    return 0;
 }
