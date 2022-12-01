1f5d9dc52357780a48818cc7c1f3f3c0f196d939
Lorenzoscajola <117931214+Lorenzoscajola@users.noreply.github.com>
2022-12-01 11:41:19 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
+#include "lib.h"
 
+int funzione(int a, int i){
+    if (a%i==0){
+        if (a==i)
+        return 1;
+        else
+            return 0;}
+    else
+        funzione(a, i+1);
+}
